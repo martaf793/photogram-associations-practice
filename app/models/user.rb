3555ef2392073c404fd978bb2ec1555f370d1c6f
martaf793 <martaf@uchicago.edu>
2025-05-12 03:43:58 +0000

    worse_after_scoped

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,16 +28,16 @@ class User < ApplicationRecord
   # User#likes: returns rows from the likes table associated to this user by the fan_id column
   has_many(:likes, class_name:"Like", foreign_key: "fan_id") 
   # User#sent_follow_requests: returns rows from the follow requests table associated to this user by the sender_id column
-  has_many(:follow_request, class_name:"FollowRequest", foreign_key: "sender_id") 
+  has_many(:sent_follow_request, class_name:"FollowRequest", foreign_key: "sender_id") 
   # User#received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column
-  has_many(:follow_request, class_name:"FollowRequest", foreign_key: "recipient_id")
+  has_many(:received_follow_request, class_name:"FollowRequest", foreign_key: "recipient_id")
 
   ### Scoped direct associations
   # User#accepted_sent_follow_requests: returns rows from the follow requests table associated to this user by the sender_id column, where status is 'accepted'
 
+    scope (:accepted_sent_follow_requests, -> {where(status: "accepted")}, class_name: "FollowRequest", foreign_key: :sender_id) #status: "accepted"
   # User#accepted_received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column, where status is 'accepted'
-
-
+  scope (:accepted_received_follow_requests, -> {where(status: "accepted")}, class_name: "FollowRequest", foreign_key: :recipient_id)
   ## Indirect associations
 
   # User#liked_photos: returns rows from the photos table associated to this user through its likes
@@ -48,10 +48,16 @@ has_many(:commented_photos, through: :comments, source: :photo)
   ### Indirect associations built on scoped associations
 
   # User#followers: returns rows from the users table associated to this user through its accepted_received_follow_requests (the follow requests' senders)
+  has_many(:followers, through: :accepted_received_follow_requests, source: :sender) #sender, not user 
 
   # User#leaders: returns rows from the users table associated to this user through its accepted_sent_follow_requests (the follow requests' recipients)
-
+  has_many(:leaders, through: :accepted_sent_follow_requests, source: :recipient)  
+  
   # User#feed: returns rows from the photos table associated to this user through its leaders (the leaders' own_photos)
+  has_many(:feed, through: :leaders, source: :own_photos) #own_photos, no photo
 
+  # scope :leaders_, -> {where(sender_id: true)}
   # User#discover: returns rows from the photos table associated to this user through its leaders (the leaders' liked_photos)
+    has_many(:discover, through: :leaders, source: :liked_photos) 
+  # scope :, -> {where(sender_id: true)}
 end
